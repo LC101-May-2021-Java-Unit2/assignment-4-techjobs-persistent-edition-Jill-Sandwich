81c65174c1f3319716765f2fdb3af2a4fb54dd0d
Katie Dessain <JillSandwich@example.com>
2021-11-01 19:09:29 -0500

    completed part 1 of setup and part 2 of persisting Employer and Skill with all tests passing

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,30 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotNull(message = "Location is required")
+    @Size(max = 100)
+    private String location;
 
+    public Employer(String aLocation){
+        super();
+        this.location = aLocation;
+    }
+
+    public Employer() {
+
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,30 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @NotNull(message = "Description is required")
+    @Size(max = 300)
+    private String description;
+
+    public Skill(String aDescription){
+        super();
+        this.description = aDescription;
+    }
+
+    public Skill() {
+
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
